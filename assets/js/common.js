4968e8e1baa10d8e787c18c926bd18d1bf24beeb
Anton Shpylenia <anton.shpylenia@sixt.com>
2017-03-16 23:50:36 +0200

    initial

---
+++ assets/js/common.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+  document.getElementById("image").addEventListener("change", uploadFileToServer, false);
+  $('#position').change(function (e) {
+    $('.image-preview').css({
+      backgroundPosition: e.target.value
+    })
+  });
+});
+
+function readFile() {
+  if (this.files && this.files[0]) {
+    var FR = new FileReader();
+    FR.onload = function (e) {
+      return e.target.result;
+    };
+    FR.readAsDataURL(this.files[0]);
+  }
+}
+
+function uploadFileToServer(event) {
+  var file = event.srcElement.files[0];
+  console.log(file);
+  var reader = new FileReader();
+  reader.readAsBinaryString(file);
+
+  reader.onload = function() {
+    var base64 = btoa(reader.result);
+    $('.image-preview').css({
+      backgroundImage: 'url(data:'+ file.type + ';base64,' + base64 + ')'
+    })
+  };
+  reader.onerror = function() {
+    console.log('there are some problems');
+  };
+}
